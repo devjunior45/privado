ee7de9cdafd8b4dba9dfe3cd3980d1b3fde5e449
Lourival junior <lourivalj782@gmail.com>
2025-10-24 09:30:45 -0400

    Update settings-sheet.tsx

--- components/ui/settings-sheet.tsx
+++ components/ui/settings-sheet.tsx
@@ -18,7 +18,7 @@ export function SettingsSheet() {
       <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger asChild>
           <Button variant="ghost" size="sm" className="p-2">
-            {/* Settings icon removed */}
+            <Settings className="w-5 h-5 mr-3" />
           </Button>
         </SheetTrigger>
         <SheetContent side="bottom" className="h-auto">
@@ -31,7 +31,7 @@ export function SettingsSheet() {
               onClick={handleOpenSettings}
               className="w-full justify-start text-left h-12 bg-transparent"
             >
-              {/* Settings icon removed */}
+              <Settings className="w-5 h-5 mr-3" />
               Configurações
             </Button>
           </div>
