a4f7a4672166d1112752afeddd8d59be0bfaca79
Lourival junior <lourivalj782@gmail.com>
2025-11-04 08:46:01 -0400

    Update sendDaily.js

--- api/sendDaily.js
+++ api/sendDaily.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(process.env.URL_SUPABASE, process.env.SUPABASE_ANON_KEY);
 
 export default async function handler(req, res) {
   try {
