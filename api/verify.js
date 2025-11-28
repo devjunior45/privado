2f2962144790596a4c534d776c10788428b3df1d
Lourival junior <lourivalj782@gmail.com>
2025-11-28 01:46:59 -0400

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,22 +1,14 @@
 export default function handler(req, res) {
   try {
-    const { token, type, redirect_to } = req.query;
+    const { url } = req.query;
 
-    if (!token || !type) {
-      return res.status(400).send("Missing token or type");
+    if (!url) {
+      return res.status(400).send("Missing URL");
     }
 
-    const SUPABASE_PROJECT_URL = "https://tdotbucnazgversgbake.supabase.co";
+    // Apenas repassa 100% do link original do Supabase
+    return res.redirect(302, url);
 
-    const finalUrl =
-      `${SUPABASE_PROJECT_URL}/auth/v1/verify?` +
-      `token=${encodeURIComponent(token)}` +
-      `&type=${encodeURIComponent(type)}` +
-      (redirect_to ? `&redirect_to=${encodeURIComponent(redirect_to)}` : "");
-
-    console.log("🔁 Redirecting to:", finalUrl);
-
-    return res.redirect(302, finalUrl);
   } catch (err) {
     console.error("Redirect error:", err);
     return res.status(500).send("Internal Server Error");
